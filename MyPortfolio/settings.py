d28afcc443f5406ec63c7ee3dfea1c100c3bf2cd
Christy Methila A <81009199+jacy28@users.noreply.github.com>
2025-09-30 21:18:26 +0530

    Update settings.py
    Chnage DEBUG = False and also set allowed host

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=ncw0^g*s2(!1=cpe91_^^a_z#1-fu%x7cdr69tbc$a%quow^!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
